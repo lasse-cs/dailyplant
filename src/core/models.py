c424ef0c1a9eaa07db0184f48533207748453f7d
lasse-cs <lasse.schmieding@gmail.com>
2026-06-05 20:57:46 +0100

    Add footer with social media links

--- src/core/models.py
+++ src/core/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
+from wagtail.admin.panels import InlinePanel
 from wagtail.fields import RichTextField
 from wagtail.images import get_image_model_string
-from wagtail.models import Page
+from wagtail.models import Orderable, Page
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
 
 
@@ -45,3 +49,30 @@ class ContentPage(Page):
     content_panels = Page.content_panels + [
         "body",
     ]
+
+
+@register_setting
+class SocialMediaSettings(BaseSiteSetting, ClusterableModel):
+    panels = [InlinePanel("social_links")]
+
+
+class SocialMediaChoices(models.TextChoices):
+    BLUESKY = "bluesky", "BlueSky"
+    FEED = "feed", "Feed"
+
+
+class SocialMediaLink(Orderable):
+    social_settings = ParentalKey(
+        SocialMediaSettings, on_delete=models.CASCADE, related_name="social_links"
+    )
+    display = models.CharField(
+        help_text="The display text of this social media link",
+        max_length=60,
+    )
+    url = models.URLField(help_text="The URL of this social media link")
+    type = models.CharField(
+        choices=SocialMediaChoices.choices,
+        default=SocialMediaChoices.BLUESKY,
+        help_text="The type of social media link",
+        max_length=32,
+    )
