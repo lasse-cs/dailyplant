65ec0128540bd754117268b28156249734514fbf
lasse-cs <lasse.schmieding@gmail.com>
2026-06-07 20:44:20 +0100

    Add Upcoming Facts Summary Item

--- src/facts/wagtail_hooks.py
+++ src/facts/wagtail_hooks.py
@@ -1,6 +1,9 @@
 from wagtail import hooks
+from wagtail.admin.auth import user_has_any_page_permission
+from wagtail.admin.site_summary import SummaryItem
 from wagtail.snippets.models import register_snippet
 
+from facts.models import FactPage
 from facts.viewsets import fact_page_viewset, FactTagSnippetViewSet, FactCalendarViewSet
 
 
@@ -15,3 +18,25 @@ register_snippet(FactTagSnippetViewSet)
 @hooks.register("register_admin_viewset")
 def register_fact_calendar_viewset():
     return FactCalendarViewSet()
+
+
+class UpcomingFactsSummaryItem(SummaryItem):
+    order = 100
+    template_name = "non_patterns/facts/admin/upcoming_facts.html"
+
+    def get_context_data(self, parent_context):
+        context = super().get_context_data(parent_context)
+        context["upcoming"] = (
+            FactPage.objects.filter(_revisions__approved_go_live_at__isnull=False)
+            .distinct()
+            .count()
+        )
+        return context
+
+    def is_shown(self):
+        return user_has_any_page_permission(self.request.user)
+
+
+@hooks.register("construct_homepage_summary_items")
+def construct_homepage_summary_items(request, items):
+    items.append(UpcomingFactsSummaryItem(request))
