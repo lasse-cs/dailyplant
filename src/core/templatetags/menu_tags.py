d951fbbf8236b70d864e5fe9289bc0b6728b8f32
lasse-cs <lasse.schmieding@gmail.com>
2026-08-02 07:43:32 +0100

    Add aria-current on current page in menu

--- src/core/templatetags/menu_tags.py
+++ src/core/templatetags/menu_tags.py
@@ -3,15 +3,20 @@ from django.template import Library
 register = Library()
 
 
-@register.inclusion_tag("patterns/components/menu/main_menu.html")
-def main_menu(site):
+@register.inclusion_tag("patterns/components/menu/main_menu.html", takes_context=True)
+def main_menu(context, site):
     root_page = site.root_page
     menu_items = root_page.get_children().live().in_menu()
     return {
         "menu_items": menu_items,
+        "request": context.get("request"),
     }
 
 
-@register.inclusion_tag("non_patterns/core/main_menu.md")
-def markdown_main_menu(site):
-    return main_menu(site)
+@register.inclusion_tag("non_patterns/core/main_menu.md", takes_context=True)
+def markdown_main_menu(context, site):
+    items = main_menu(context, site)
+    # Might want to think about this... need specific information to
+    # check if the page supports markdown or not...
+    items["menu_items"] = items["menu_items"].specific()
+    return items
--- src/patterns/templates/non_patterns/core/main_menu.md
+++ src/patterns/templates/non_patterns/core/main_menu.md
@@ -1,4 +1,4 @@
-{% load wagtailcore_tags %}
+{% load markdown_tags %}
 {% for menu_item in menu_items %}
-1. [{{ menu_item.title }}]({% pageurl menu_item %})
+1. [{{ menu_item.title }}]({% markdownpageurl menu_item %})
 {% endfor %}
