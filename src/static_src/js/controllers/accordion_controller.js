2414de31c2acc8bac93184df89a230ac0b59c509
lasse-cs <lasse.schmieding@gmail.com>
2026-08-13 06:27:25 +0100

    Table styles

--- src/static_src/js/controllers/accordion_controller.js
+++ src/static_src/js/controllers/accordion_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static classes = ["active"];
-
     toggle() {
         const controlledElement = document.getElementById(
             this.element.getAttribute("aria-controls"),
@@ -11,10 +9,6 @@ export default class extends Controller {
         this.element.setAttribute(
             "aria-expanded",
             String(!controlledElement.hidden),
-        );
-        this.element.classList.toggle(
-            this.activeClass,
-            !controlledElement.hidden,
         );
     }
 }
